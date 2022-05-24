aca60b446ccc6c97c09644f5c211d46b3b82411f
Omar Shaheen <16275929+omar-shaheen@users.noreply.github.com>
2022-05-24 02:22:06 +0200

    First
    First

--- crud-redux-toolkit/crup-app/src/redux/postsSlice.js
+++ crud-redux-toolkit/crup-app/src/redux/postsSlice.js
@@ -10,19 +10,19 @@ export const postsSlice = createSlice({
       // console.log(action);
       state.items.push(action.payload);
     },
-    
+
     deletePost: function (state, action) {
       state.items = state.items.filter((item) => item.id != action.payload);
     },
 
     updatePost: (state, action) => {
-      state.items.map(item=> {
-        if(item.id == action.payload.id) {
+      state.items.map((item) => {
+        if (item.id == action.payload.id) {
           item.title = action.payload.title;
           item.desc = action.payload.desc;
         }
-      })
-    }
+      });
+    },
   },
 });
 
